5cff782738a448873610cf0b65b5e9873f123765
Andrew Smith <andrew.smith@aeriandi.com>
2022-02-21 21:31:11 +0000

    SvgGenerator

---
+++ SvgGenerator/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SvgGenerator;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        Trace.Listeners.Add(new TextWriterTraceListener(Console.Out));
+        
+        Console.WriteLine("Hello, World!");
+    }
+}
